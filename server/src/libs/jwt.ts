e8efaaae6aa498332c204abe2311caf7e23a4a09
Nefta Hernandez <neftaligow01@gmail.com>
2024-04-11 16:11:53 -0600

    Add JWT token creation logic in createAccessToken function

---
+++ server/src/libs/jwt.ts
@@ -0,0 +1,10 @@
+// ✅ TODO: Recibir un payload y devolver un token en una funcion async
+//  TODO: Retornar una promesa con el token en el resolve
+//  TODO: Manejar el error en el reject
+import jwt from "jsonwebtoken";
+
+export const createAccesToken = async (payload: string) => {
+    return new Promise((resolve, reject) => {
+                
+    })
+} 
