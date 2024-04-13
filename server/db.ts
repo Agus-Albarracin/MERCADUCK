8fc8ff073e1807b5f829d830998c6115021aae39
Agus-Albarracin <abusscin_@hotmail.com>
2024-04-13 04:11:00 -0300

    Modified db url, its now the DB is on cloud since mongo Atlas

--- server/db.ts
+++ server/db.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async (force = false) => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/Mercaduck");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/Mercaduck");
+    await mongoose.connect("mongodb+srv://danyelmartel:12345@ecommerce.a2mniiw.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce");
+
     console.log("Se inicio MongoDB correctamente");
 
     if (force) {
@@ -19,3 +21,4 @@ const connectDB = async (force = false) => {
 };
 
 export default connectDB;
+
