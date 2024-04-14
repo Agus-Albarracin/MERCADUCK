2fce66e4fb90db2f5a7dad174de15ebd9d45dc8d
Agus-Albarracin <abusscin_@hotmail.com>
2024-04-14 09:18:13 -0300

    Integration of Auth in Home

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId="702210968252-3h3f91te0phli4rl0h497219ce7ud06a.apps.googleusercontent.com">
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </GoogleOAuthProvider>
 )
