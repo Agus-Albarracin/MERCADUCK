3ca773923fff3abd6d635c56e2eda080c495f1c7
guscedgua <gustavocedeno@gmail.com>
2024-04-22 14:46:20 -0400

    primer commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
+import { constants } from 'buffer';
 import './App.css'
 import Auth from "./helpers/googleLogin/GoogleOAuth"
 import axios from "axios";
+import { useState } from 'react';
 axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
+const [count, setCount] = useState(0)
 
-  return (
-    <>
-    <Auth />
-    </>
+return (
+    <div>
+    <header>TS AMAZONIAA</header>
+    <main></main>
+    <Auth />´
+    <footer>ALL RIGTH  RESERVE</footer>
+
+    </div>
   )
 }
 
