b24bf0974cc48d87b788fc74745a2ca571ff2894
cterryc <danyel.martel@gmail.com>
2024-04-22 14:21:21 -0500

    landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,12 @@ import './App.css'
 import Auth from './helpers/googleLogin/GoogleOAuth'
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3001'
+import Landing from './page/landing.tsx'
 
 function App() {
   return (
     <>
+      <Landing />
       <Auth />
     </>
   )
