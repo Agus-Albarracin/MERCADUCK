0bbb6f8caad0774f9ade31c939b2aef9e3338cc5
Agus-Albarracin <abusscin_@hotmail.com>
2024-04-25 12:16:01 -0300

    pull updates changes

--- client/src/helpers/googleLogin/GoogleOAuth.tsx
+++ client/src/helpers/googleLogin/GoogleOAuth.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
 import xpicker from 'cactus-google-decode/cli'; 
 
-const  GoogleLoginComponent: React.FC = () => {
+const GoogleLoginComponent: React.FC = () => {
   /* 
     PRO: this have support in react-oauth/google
          this a new version and current component.
@@ -21,31 +21,31 @@ const  GoogleLoginComponent: React.FC = () => {
       console.log(credentialResponse);
     const res = xpicker("http://localhost:3001/api/signin", credentialResponse)
     console.log(res)
+    }
 
-    };
-  
-    const onError = () => {
-      console.log('Login Failed');
-    };
 
-    return (
-        <>
-          <GoogleLogin
-            onSuccess={onSuccess}
-            onError={onError}
-            useOneTap={true}
-            type="standard"
-            theme="filled_black"
-            size="medium"
-            text="continue_with"
-            shape="pill"
-            logo_alignment="left"
-            width="200px"
-            locale="es"
-            use_fedcm_for_prompt={true}
-          />
-        </>
-    );
-};
+  const onError = () => {
+    console.log('Login Failed')
+  }
+    
+  return (
+    <>
+      <GoogleLogin
+        onSuccess={onSuccess}
+        onError={onError}
+        useOneTap={true}
+        type='standard'
+        theme='filled_black'
+        size='medium'
+        text='continue_with'
+        shape='pill'
+        logo_alignment='left'
+        width='200px'
+        locale='es'
+        use_fedcm_for_prompt={true}
+      />
+    </>
+  )
+}
 
-export default GoogleLoginComponent;
+export default GoogleLoginComponent
---
+++ client/src/page/landing.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../componentes/Nav/Navbar'
+
+const Landing = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Landing
